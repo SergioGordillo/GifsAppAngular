dff28383802ab03a86edebe9b6c9c83936f5de2a
Sergio Gordillo <sergiogordillodev@gmail.com>
2021-06-21 00:36:41 +0200

    Created storage of the data searched in the sidebar

--- src/app/gifs/search-gifs/search-gifs.component.ts
+++ src/app/gifs/search-gifs/search-gifs.component.ts
@@ -1,18 +1,20 @@
 import { Component, ElementRef, ViewChild} from '@angular/core';
+import { GifsService } from '../services/gifs.service';
 
 @Component({
   selector: 'app-search-gifs',
   templateUrl: './search-gifs.component.html',
-  styles: [
-  ]
 })
 export class SearchGifsComponent{
 
 @ViewChild('txtSearch') txtSearch!:ElementRef<HTMLInputElement>;
 
+constructor(private gifsService:GifsService){}
+
 search(){
 
   const value=this.txtSearch.nativeElement.value;
+  this.gifsService.searchGifs(value);
   this.txtSearch.nativeElement.value='';
 }
 

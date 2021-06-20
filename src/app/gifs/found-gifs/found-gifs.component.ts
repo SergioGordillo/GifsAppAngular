39d2f546bdbe09a0accbfd51205bbb3e6ccefce1
Sergio Gordillo <sergiogordillodev@gmail.com>
2021-06-20 18:36:38 +0200

    Modules and components of the app created

---
+++ src/app/gifs/found-gifs/found-gifs.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-found-gifs',
+  templateUrl: './found-gifs.component.html',
+  styles: [
+  ]
+})
+export class FoundGifsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/gifs/search-gifs/search-gifs.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-gifs',
+  templateUrl: './search-gifs.component.html',
+  styles: [
+  ]
+})
+export class SearchGifsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

d1cf72009459f9f9411b2b54cdc929236493f529
Sergio Gordillo <sergiogordillodev@gmail.com>
2021-06-24 13:12:41 +0200

    Search history saved in localStorage

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient} from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gifs, SearchGifsResponse } from '../search-gifs/interfaces/search-gifs.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -7,17 +8,18 @@ import { Injectable } from '@angular/core';
 export class GifsService {
 
   private api_key: string="Lc0u9E4rl7pjIsJdbAdGS1W0DOgGfu1E"
-  // https://api.giphy.com/v1/gifs/search?api_key=Lc0u9E4rl7pjIsJdbAdGS1W0DOgGfu1E&q=madrid&limit=10
   private _history: string[]=[];
 
-  public results: any[]=[];
+  public results: Gifs[]=[];
   
 
   get history(){
     return [...this._history];
   }
 
-  constructor(private http: HttpClient){}
+  constructor(private http: HttpClient){
+    this._history=JSON.parse(localStorage.getItem("history")!) || [];
+  }
 
   searchGifs(query:string=''){
     
@@ -26,11 +28,13 @@ export class GifsService {
     if(!this._history.includes(query)){
       this._history.unshift(query);
       this._history=this._history.splice(0,10);
+
+      localStorage.setItem("history", JSON.stringify(this._history));
     }
 
 
-    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=Lc0u9E4rl7pjIsJdbAdGS1W0DOgGfu1E&q=${query}&limit=10`)
-    .subscribe((resp:any)=>{
+    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=Lc0u9E4rl7pjIsJdbAdGS1W0DOgGfu1E&q=${query}&limit=10`)
+    .subscribe((resp)=>{
       console.log(resp.data);
       this.results=resp.data;
     })
